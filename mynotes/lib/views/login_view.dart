260489c923cf9d92ef3153cf2c6ff7631f106c64
Shubha Pradhan <shubhadon7@gmail.com>
2022-05-16 09:51:39 +0545

    changing to AUTH service

--- mynotes/lib/views/login_view.dart
+++ mynotes/lib/views/login_view.dart
@@ -1,6 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/services/auth/auth_exceptions.dart';
+import 'package:mynotes/services/auth/auth_service.dart';
 import '../utilities/show_error_dialog.dart';
 class LoginView extends StatefulWidget {
   const LoginView({ Key? key }) : super(key: key);
@@ -17,14 +18,14 @@ class _LoginViewState extends State<LoginView> {
   @override
   void initState() {
     _email = TextEditingController();
-    _password = TextEditingController();// TODO: implement initState
+    _password = TextEditingController();
     super.initState();
   }
 
   @override
   void dispose() {
     _email.dispose();
-    _password.dispose();// TODO: implement dispose
+    _password.dispose();
     super.dispose();
   }
   
@@ -59,29 +60,20 @@ class _LoginViewState extends State<LoginView> {
                   final email = _email.text;
                   final password = _password.text;
                   try {
-                    await FirebaseAuth.instance.signInWithEmailAndPassword(
-                    email: email,
-                    password: password,
-                  );
-                  final user = FirebaseAuth.instance.currentUser;
-                  if (user?.emailVerified ?? false) {
+                    await AuthService.firebase().logIn(email: email, password: password);
+                  final user = AuthService.firebase().currentUser;
+                  if (user?.isEmailVerified ?? false) {
                     Navigator.of(context).pushNamedAndRemoveUntil(notesRoute, (route) => false);
                   }
                   else {
                     Navigator.of(context).pushNamedAndRemoveUntil(verifyEmailRoute, (route) => false);
                   }
-                  } on FirebaseAuthException catch (e) {
-                    if (e.code == 'user-not-found') {
-                      await showErrorDialog(context, "User not found");
-                    }
-                    else if (e.code == 'wrong-password') {
-                      await showErrorDialog(context, "Wrong password");
-                    }
-                    else {
-                      await showErrorDialog(context, "Error: ${e.code}");
-                    }
-                  } catch (e) {
-                    await showErrorDialog(context, "Error: ${e.toString()}");
+                  } on UserNotFoundAuthException {
+                    await showErrorDialog(context, 'User not found');
+                  } on WrongPasswordAuthException {
+                    await showErrorDialog(context, "Wrong password");
+                  } on GenericAuthException {
+                    await showErrorDialog(context, "Authentication error");
                   }
                 }, 
                 child: const Text('Login'),
--- mynotes/lib/views/register_view.dart
+++ mynotes/lib/views/register_view.dart
@@ -1,6 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/services/auth/auth_exceptions.dart';
+import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/utilities/show_error_dialog.dart';
 class RegisterView extends StatefulWidget {
   const RegisterView({ Key? key }) : super(key: key);
@@ -17,14 +18,14 @@ class _RegisterViewState extends State<RegisterView> {
   @override
   void initState() {
     _email = TextEditingController();
-    _password = TextEditingController();// TODO: implement initState
+    _password = TextEditingController();
     super.initState();
   }
 
   @override
   void dispose() {
     _email.dispose();
-    _password.dispose();// TODO: implement dispose
+    _password.dispose();
     super.dispose();
   }
 
@@ -59,26 +60,19 @@ class _RegisterViewState extends State<RegisterView> {
                   final email = _email.text;
                   final password = _password.text;
                   try {
-                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                    email: email,
-                    password: password,
-                  );
-                  final user = FirebaseAuth.instance.currentUser;
-                  await user?.sendEmailVerification();
-                  Navigator.of(context).pushNamed(verifyEmailRoute);
-                  } on FirebaseAuthException  catch (e) {
-                    if (e.code == 'weak-password') {
-                      await showErrorDialog(context, "Weak Password");
-                    } else if (e.code == 'email-already-in-use') {
-                       await showErrorDialog(context, "Email already in use");
-                    } else if (e.code == 'invalid-email') {
-                       await showErrorDialog(context, "Invalid email");
-                    } else {
-                       await showErrorDialog(context, "Error: ${e.code}");
+                    await AuthService.firebase().createUser(email: email, password: password);
+                    AuthService.firebase().currentUser;
+                    AuthService.firebase().sendEmailVerification();
+                    Navigator.of(context).pushNamed(verifyEmailRoute);
+                    } on WeakPasswordAuthException {
+                      await showErrorDialog(context, 'Weak Password');
+                    } on EmailAlreadyInUseAuthException {
+                      await showErrorDialog(context, 'Email already in use');
+                    } on InvalidEmailAuthException {
+                      await showErrorDialog(context, 'Invalid email');
+                    } on GenericAuthException {
+                      await showErrorDialog(context, 'Failed to register');
                     }
-                  } catch (e) {
-                    await showErrorDialog(context, "Error: ${e.toString()}");
-                  }
                 }, 
                 child: const Text('Register'),
               ),
